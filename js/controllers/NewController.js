25c3eb5196030b961d0fb128a5f70314a7ff8bcf
Maria <mmorrison1978@gmail.com>
2016-03-06 23:28:37 -0700

    added models to form

--- js/controllers/NewController.js
+++ js/controllers/NewController.js
@@ -1,6 +1,7 @@
-app.controller('NewController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService){
-  resources = $routeParams.resource;
-  $scope.works='this is the new page and the new controller is working'
-
-    
-}])
+app.controller("NewController", function($scope, MyService, $routeParams) {
+    $scope.resource = {};
+    $scope.newResource = function(){
+      MyService.newResource($scope.resource).then(function() {
+        })
+      }
+  });
--- js/services/MyService.js
+++ js/services/MyService.js
@@ -1,25 +1,17 @@
 app.service("MyService", function($http){
 
   return {
-    
+
     getter: function(resources) {
       return $http.get('https://apiandapp.herokuapp.com/')
     },
+
+    newResource: function(resource) {
+      return $http.post('https://apiandapp.herokuapp.com/')
+    }
 
 
 
 
   }
 });
-
-
-// app.service('MyService', ['$http', function($http) {
-//   return {
-//     getter: function(search) {
-//        return $http.get("http://www.omdbapi.com/?s=" + search);
-//     },
-//     oneMovie: function(movieID) {
-//       return $http.get("http://www.omdbapi.com/?i=" + movieID + "&plot=full");
-//     }
-//   }
-// }]);
