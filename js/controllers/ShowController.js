c36a1d7f79b1099c09c8125a82d6929092ddd598
Maria <mmorrison1978@gmail.com>
2016-03-07 00:05:34 -0700

    working on show page

--- js/controllers/ShowController.js
+++ js/controllers/ShowController.js
@@ -1,8 +1,10 @@
 app.controller("ShowController", function($scope, MyService, $routeParams){
     id = $routeParams.id;
+    console.log('show controller is working');
 
     MyService.getOne(id).then(function(payload){
       $scope.singleResource = payload.data
+      console.log(payload);
     }, function(error){
       console.log("an error occurred");
     });
