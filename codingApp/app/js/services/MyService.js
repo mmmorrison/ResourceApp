e791fd9aec29d8288726302bc28d5b4da8b2aad6
Maria <mmorrison1978@gmail.com>
2016-03-06 19:37:18 -0700

    added ResourcesController

--- codingApp/app/js/services/MyService.js
+++ codingApp/app/js/services/MyService.js
@@ -1,22 +1,25 @@
 app.service("MyService", function($http){
-  var CatService = {};
 
-  // MyService.getCats = function(){
-  //   return $http.get("http://localhost:3000/cats", {method: "jsponp"});
-  //   // return $http.get("https://frozen-badlands-34577.herokuapp.com/cats", {method: "jsonp"});
-  // }
-  //
-  // MyService.getCat = function(cat_id){
-  //   return $http.get("http://localhost:3000/cats/"+cat_id);
-  //   // return $http.get("https://frozen-badlands-34577.herokuapp.com/cats/"+cat_id, {method: "jsonp"});
-  // }
-  //
-  // MyService.newCat = function(cat_object) {
-  //   console.log("reached the service and creating a new cat");
-  //   return $http.post('http://localhost:3000/cats/', cat_object)
-  //     /// needs to take in data from the from (my_data is what we're calling it here, )
-  //     ///  and it needs to make a post request to our backend API
-  // }
+  return {
+    
+    getter: function(resources) {
+      return $http.get('https://apiandapp.herokuapp.com/')
+    },
 
-  return MyService;
+
+
+
+  }
 });
+
+
+// app.service('MyService', ['$http', function($http) {
+//   return {
+//     getter: function(search) {
+//        return $http.get("http://www.omdbapi.com/?s=" + search);
+//     },
+//     oneMovie: function(movieID) {
+//       return $http.get("http://www.omdbapi.com/?i=" + movieID + "&plot=full");
+//     }
+//   }
+// }]);
