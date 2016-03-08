0dde9998e7e29d9573a32f55680938165dc06c00
Maria <mmorrison1978@gmail.com>
2016-03-08 15:01:35 -0700

    show page working

--- js/controllers/ShowController.js
+++ js/controllers/ShowController.js
@@ -2,7 +2,7 @@ app.controller("ShowController", function($scope, MyService, $routeParams){
     id = $routeParams.id;
 
     MyService.getOne(id).then(function(payload){
-      $scope.resource = payload.data[0]
+      $scope.resource = payload.data
       console.log(payload.data);
     }, function(error){
       console.log("an error occurred");
