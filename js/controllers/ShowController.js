5ff09a772def27197b750803c71ee4c47ddc42ca
Maria <mmorrison1978@gmail.com>
2016-03-12 23:44:16 -0700

    adding comments

--- js/controllers/ShowController.js
+++ js/controllers/ShowController.js
@@ -4,7 +4,7 @@ app.controller("ShowController", function($scope, MyService, $routeParams){
     MyService.getComments(id).then(function(payload){
       $scope.resource = payload.data
       $scope.comments = payload.data.comments
-      console.log('refreshed3');
+      console.log('refreshed4');
     }, function(error){
       console.log("an error occurred");
     });
