38549d65d005814b8a5ea08a07878fdccc7771db
Maria <mmorrison1978@gmail.com>
2016-03-13 11:18:48 -0600

    removed logs

--- js/controllers/ShowController.js
+++ js/controllers/ShowController.js
@@ -4,8 +4,6 @@ app.controller("ShowController", function($scope, MyService, $routeParams){
     MyService.getComments(id).then(function(payload){
       $scope.resource = payload.data
       $scope.comments = payload.data.comments
-      console.log($scope.comments);
-      console.log('refreshed5');
     }, function(error){
       console.log("an error occurred");
     });
